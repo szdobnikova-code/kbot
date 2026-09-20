afe564245537ca73e4e8362862bc21610071dee8
Sofia Zdobnikova <szdobnikova@gmail.com>
2026-09-20 15:05:25 +0300

    Align Jenkins pipeline with parameterized build example

--- pipeline/jenkins.groovy
+++ pipeline/jenkins.groovy
@@ -7,49 +7,20 @@ pipeline {
             choices: ['linux', 'darwin', 'windows'],
             description: 'Target operating system'
         )
-
         choice(
             name: 'ARCH',
             choices: ['amd64', 'arm64'],
             description: 'Target architecture'
         )
-
         booleanParam(
             name: 'SKIP_TESTS',
             defaultValue: false,
             description: 'Skip running tests'
         )
-
         booleanParam(
             name: 'SKIP_LINT',
             defaultValue: false,
             description: 'Skip running linter'
         )
-    }
-
-    stages {
-        stage('Test') {
-            when {
-                expression { !params.SKIP_TESTS }
-            }
-            steps {
-                sh 'make test'
-            }
-        }
-
-        stage('Lint') {
-            when {
-                expression { !params.SKIP_LINT }
-            }
-            steps {
-                sh 'make lint'
-            }
-        }
-
-        stage('Build') {
-            steps {
-                sh "make build TARGETOS=${params.OS} TARGETARCH=${params.ARCH}"
-            }
-        }
     }
 }
